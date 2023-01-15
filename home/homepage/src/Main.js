488b1f82734fc71bbbc0714581bf292348bcd2e2
Pasindu-N-01 <pasindunishshanka82@gmail.com>
2023-01-15 21:59:46 +0530

    updated

--- home/homepage/src/Main.js
+++ home/homepage/src/Main.js
@@ -16,9 +16,11 @@ class Main extends Component {
         <div>
           <h1>Simple SPA</h1>
           <ul className="header">
-            <li><a href="/">Home</a></li>
-            <li><a href="/stuff">Stuff</a></li>
-            <li><a href="/contact">Contact</a></li>
+
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/stuff">Stuff</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+
           </ul>
 <div className="content">
 
