dfe4dbbc52e0beee2d9ef56f2fa2702f0a51acae
Pasindu-N-01 <pasindunishshanka82@gmail.com>
2023-01-15 21:56:03 +0530

    updated

--- home/homepage/src/Main.js
+++ home/homepage/src/Main.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
- import React, { Component } from "react";
 import {
-  Route,
-  NavLink,
-  HashRouter
+Route,
+NavLink,
+HashRouter
 } from "react-router-dom";
 import Home from "./Home";
 import Stuff from "./Stuff";
 import Contact from "./Contact";
+
 class Main extends Component {
   render() {
     return (
